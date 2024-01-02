dfe74639386245d56196272bc007f8c9862d2d5d
Luc Demierre <luc@d-side.pro>
2024-01-02 18:11:05 +0100

    fix datatables

--- config/app.php
+++ config/app.php
@@ -168,7 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        
+        Yajra\DataTables\DataTablesServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
     ])->toArray(),
 
@@ -186,8 +186,7 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,        
         'PDF' => Barryvdh\DomPDF\Facade\Pdf::class,
-
-
+        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
     ])->toArray(),
     
 
