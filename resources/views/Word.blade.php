2d65c796e78b09277165f9bd047047f4efe31a5c
Luc Demierre <luc@d-side.pro>
2024-01-03 12:00:36 +0100

    Fix hard coded app title

--- resources/views/Word.blade.php
+++ resources/views/Word.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
 
