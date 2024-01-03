4425f0ca32a83456106b614ceca1664f43dd340f
Luc Demierre <luc@d-side.pro>
2024-01-03 11:40:00 +0100

    init

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Lang;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Lang::handleMissingKeysUsing(function (string $key, array $replacements, string $locale) {
+            info("Missing translation key [$key] detected.");
+     
+            return $key;
+        });
     }
 }
---
+++ lang/en/messages.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+
+    
+    'dashboard' => 'Dashboard',
+    'words' => 'Words',
+    'word_table' => 'Word table',
+    'manage_books' => 'Manage books',
+    'word_no' => 'N.',
+    'word_title' => 'Word',
+    'word_add' => 'Add a word',
+    'word_level' => 'Level',
+    'word_lang' => 'Langage',
+    'word_action' => 'Action',
+    'word_action_view' => 'View',
+    'word_action_edit' => 'Edit',
+    'book_view_title' => 'Book mamagment',
+    'book_table' => 'Books list',
+    'book_no' => 'No.',
+    'book_title' => 'Title',
+    'book_level' => 'Level',
+    'book_action' => 'Action',
+    'book_add' => 'Add a book',
+
+];
