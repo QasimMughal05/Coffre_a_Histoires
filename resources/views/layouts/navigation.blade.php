aba8d6747492fd113a90b1621119aa3247da0bbd
Luc Demierre <luc@d-side.pro>
2024-01-03 13:30:29 +0100

    add project link to menu0

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,6 +12,9 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link href="/" :active="request()->routeIs('project')">
+                        Le Projet
+                    </x-nav-link>
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
